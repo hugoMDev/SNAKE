0c5406b2b17743e79bb4567ffc8dbdd45ae234cb
HugoMDev <hugodamasceno1990@gmail.com>
2021-12-12 17:44:06 -0300

    tick e render

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -1,4 +1,26 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Snake {
+public class Snake extends Canvas implements Runnable {
+	
+	public Snake() {
+		this.setPreferredSize(new Dimension(280,280));
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
+	}
 
+	@Override
+	public void run() {
+		while(true) {
+			tick();
+			render();
+			Thread.sleep(1000/60);
+		}
+	}
 }
