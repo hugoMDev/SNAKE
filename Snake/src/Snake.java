73332e3a3a6857976f281761f7a964d1c1c3e4ff
HugoMDev <hugodamasceno1990@gmail.com>
2021-12-12 18:49:53 -0300

    funcionalidades de movimentos

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -2,11 +2,15 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-public class Snake extends Canvas implements Runnable {
+public class Snake extends Canvas implements Runnable, KeyListener {
+	
+	public boolean right, left, up, down;
 	
 	public Node[] nodeSnake = new Node[13];
 	
@@ -15,10 +19,23 @@ public class Snake extends Canvas implements Runnable {
 		for(int i = 0; i < nodeSnake.length; i++) {
 			nodeSnake[i] = new Node(0,0);
 		}
+		this.addKeyListener(this);
 	}
 	
 	public void tick() {
-		
+		for(int i= nodeSnake.length -1; i > 0; i-- ) {
+			nodeSnake[i].x = nodeSnake[i-1].x;
+			nodeSnake[i].y = nodeSnake[i-1].y;
+		}
+		if(right) {
+			nodeSnake[0].x++;
+		}else if(left) {
+			nodeSnake[0].x--;
+		}else if(up) {
+			nodeSnake[0].y--;
+		}else if(down) {
+			nodeSnake[0].y++;
+		}
 	}
 	
 	public void render() {
@@ -63,5 +80,43 @@ public class Snake extends Canvas implements Runnable {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = true;
+			left = false;
+			up = false;
+			down = false;
+		}else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = true;
+			right = false;
+			up = false;
+			down = false;
+		}else if(e.getKeyCode() == KeyEvent.VK_UP) {
+			up = true;
+			right = false;
+			left = false;
+			down = false;
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			down = true;
+			right = false;
+			left = false;
+			up = false;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
