05c8042445b3a2dd2a3a7d77fc94d82dc9d8fcd9
HugoMDev <hugodamasceno1990@gmail.com>
2021-12-12 21:45:15 -0300

    add apple and speed system

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -2,9 +2,11 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
@@ -13,6 +15,12 @@ public class Snake extends Canvas implements Runnable, KeyListener {
 	public boolean right, left, up, down;
 	
 	public Node[] nodeSnake = new Node[13];
+	
+	public int macaX = 0, macaY = 0;
+	
+	public int score = -1;
+	
+	public int spd = 0;
 	
 	public Snake() {
 		this.setPreferredSize(new Dimension(280,280));
@@ -28,13 +36,20 @@ public class Snake extends Canvas implements Runnable, KeyListener {
 			nodeSnake[i].y = nodeSnake[i-1].y;
 		}
 		if(right) {
-			nodeSnake[0].x++;
+			nodeSnake[0].x+=spd;
 		}else if(left) {
-			nodeSnake[0].x--;
+			nodeSnake[0].x-=spd;
 		}else if(up) {
-			nodeSnake[0].y--;
+			nodeSnake[0].y-=spd;
 		}else if(down) {
-			nodeSnake[0].y++;
+			nodeSnake[0].y+=spd;
+		}
+		if(new Rectangle(nodeSnake[0].x, nodeSnake[0].y, 13,13).intersects(new Rectangle(macaX, macaY,13,13))) {
+			macaX = new Random().nextInt(280-13);
+			macaY = new Random().nextInt(280-13);
+			score++;
+			spd++;
+			System.out.println("Pontos: " +score);
 		}
 	}
 	
@@ -51,6 +66,8 @@ public class Snake extends Canvas implements Runnable, KeyListener {
 			g.setColor(Color.yellow);
 			g.fillRect(nodeSnake[i].x, nodeSnake[i].y, 13, 13);
 		}
+		g.setColor(Color.red);
+		g.fillRect(macaX, macaY, 13, 13);
 		
 		g.dispose();
 		bs.show();
